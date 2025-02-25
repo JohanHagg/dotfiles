7ab427704afc17fc42e791037a8410d85cf07592
Johan Häggström <johan.haggstroem@gmail.com>
2025-02-25 22:36:58 +0100

    Change font size and hide single tab

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,14 +1,20 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 -- Color theme
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 
 -- Disable annoying sound bell
-config.audible_bell = 'Disabled'
+config.audible_bell = "Disabled"
 
 -- config.font = wezterm.font 'Nerd Font Symbols'
+
+-- Hide tab bar when single tab
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Font size
+config.font_size = 13.8
 
 return config
